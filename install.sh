2c72e0c089e3a193b0d34db1b62809a9c68da163
ji4a <117786715+ji4a@users.noreply.github.com>
2023-01-19 14:31:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 yum install tigervnc-server
 yum groupinstall "GNOME Desktop"
 firewall-cmd --add-port=5901/tcp --permanent
+systemctl restart firewalld.service
 
 #AFTER INSTALL EXECUTE
 #vncserver
