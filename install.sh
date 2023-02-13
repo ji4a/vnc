0597bb15fc9d7f3848cac896cee0d291b637ab27
ji4a <117786715+ji4a@users.noreply.github.com>
2023-02-12 16:35:29 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
+# INSTALL BTOP VIA SNAPD
+yum -y install snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+snap install btop
+
+# INSTALL HTOP
 yum -y install htop
+
+# INSTALL VNCSERVER
 yum -y install tigervnc-server
 yum -y groupinstall "GNOME Desktop"
 firewall-cmd --add-port=5901/tcp --permanent
 systemctl restart firewalld.service
-
 vncserver
+# GIVE PASSWD AND GO!
 
-#give passwd and go!
+
+# INSTALL VESTACP IF YOU WANT
+
+# curl -O http://vestacp.com/pub/vst-install.sh 
+# bash vst-install.sh
